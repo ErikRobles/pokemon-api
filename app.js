0015beb7c2516c8bdd9f2260b5331e5beda0a68a
ErikRobles <erikjames69@hotmail.com>
2024-06-08 16:06:55 -0600

    Added more logging details for vercel in app.js

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-    });
+    }); 
 }
-
+//Adding more logging 
 // Export the app for testing purposes
 module.exports = app;
