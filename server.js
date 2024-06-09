9851acb814ab1aabf1d079fada7c0044cadf1c46
ErikRobles <erikjames69@hotmail.com>
2024-06-09 07:26:11 -0600

    removed unnecesary server file

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-// switch from app running server to server.js due to server not closing properly for testing.
-
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = server;
